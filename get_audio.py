3dd69631eab93c1ec9e35120563f3cc0a4af6659
naveenmalla <s4namall@uni-trier.de>
2024-07-22 22:26:23 +0200

    code to download audio of given youtube url

---
+++ get_audio.py
@@ -0,0 +1,26 @@
+import os
+import yt_dlp as youtube_dl
+from dotenv import load_dotenv
+
+def download_audio_from_youtube(youtube_url, output_path='.'):
+    load_dotenv('.env')
+
+    ydl_opts = {
+        'username': os.getenv('YOUTUBE_EMAIL'), 
+        'password': os.getenv('YOUTUBE_PASSWORD'),  
+        'format': 'bestaudio/best',  
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',  # Use FFmpeg to extract audio
+            'preferredcodec': 'mp3',  # Convert audio to mp3
+            'preferredquality': '192',  # Use a bitrate of 192 kbps
+        }],
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'nocheckcertificate': True,  # Ignore SSL certificate errors
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url]) 
+
+if __name__ == '__main__':
+    youtube_url = 'https://youtu.be/LpdD1cHhGI8' 
+    download_audio_from_youtube(youtube_url)
